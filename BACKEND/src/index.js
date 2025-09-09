269c7f708b54191245e88b7d40d3473eda044700
Leon Styven <j.stca03@gmail.com>
2025-09-09 16:35:50 -0500

    Add CORS and dotenv dependencies; set up Express server

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+import  dotenv from "dotenv"; // to load the environment variables
+import express from "express"; // to create the server
+import cors from "cors"; // to handle the cors policy
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
