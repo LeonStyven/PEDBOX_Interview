27cde5d9c60825ea8908b250ef138444e4afcd41
Leon Styven <j.stca03@gmail.com>
2025-09-10 11:15:48 -0500

    [FEATURE]: Add JWT authentication middleware and user model for secure user management in the backend.

---
+++ BACKEND/src/models/user.model.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+import { DataTypes } from "sequelize";
+
+
+    const User = sequelize.define('User', {
+        id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+        name: {type: DataTypes.STRING, allowNull: false, unique: true},
+        email: {type: DataTypes.STRING, allowNull: false, unique: true},
+        password: {type: DataTypes.STRING, allowNull: false},
+    },{
+        tableName: 'users',
+        timestamps: false
+    });
+
+
+export default User;
