db61b22bd62835d166ad6add608f2694260e3cf2
Leon Styven <j.stca03@gmail.com>
2025-09-10 16:23:28 -0500

    [FEATURE]: Implement AuthGuard for route protection and add tests for authentication guard functionality.

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { SubredditsPageComponent } from './subreddit/pages/subreddits-page/subreddits-page.component';
 import { SignInPageComponent } from './user/signin/pages/sign-in-page/sign-in-page.component';
 import { LogInPageComponent } from './user/login/pages/log-in-page/log-in-page.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
     {
       path: 'subreddits',
-      component: SubredditsPageComponent
+      component: SubredditsPageComponent,
+      canActivate: [AuthGuard]
     },
     {
         path: 'auth',
