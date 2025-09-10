34b73010fc2ff2a6053fcd53e85d90707a2feaeb
Leon Styven <j.stca03@gmail.com>
2025-09-10 01:21:56 -0500

    Add RESTSubreddit and Subreddit interfaces for consistent data structure in subreddit management

---
+++ Frontend/src/app/subreddit/interfaces/RESTSubreddit.interface.ts
@@ -0,0 +1,10 @@
+export interface RESTSubreddit {
+    name: string,
+    title: string,
+    subscribers: number,
+    createdAt: number,
+    submit_text: string,
+    over18: boolean,
+    description: string,
+    lang: string
+}
---
+++ Frontend/src/app/subreddit/interfaces/subreddit.interface.ts
@@ -0,0 +1,10 @@
+export interface Subreddit {
+    name: string,
+    title: string,
+    subscribers: number,
+    createdAt: number,
+    submit_text: string,
+    over18: boolean,
+    description: string,
+    lang: string
+}
