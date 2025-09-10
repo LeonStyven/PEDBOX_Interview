012d128990e09c4f156baf86ace1e69b30b52b92
Leon Styven <j.stca03@gmail.com>
2025-09-10 00:07:55 -0500

    [FEATURE]: Add route to fetch subreddit by ID in subreddit routes

--- BACKEND/src/routes/subreddit.routes.js
+++ BACKEND/src/routes/subreddit.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getSubreddits, updateSubreddits } from "../controllers/subreddit.controller.js";
+import { getSubreddits, updateSubreddits, getSubredditById } from "../controllers/subreddit.controller.js";
 
 
 const subredditRouter = express.Router();
 
 subredditRouter.get("/subreddits", getSubreddits);
+subredditRouter.get("/subreddits/:id", getSubredditById);
+
 subredditRouter.put("/update",  updateSubreddits);
 
 
